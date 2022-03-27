6f8c0f72fd2a5fc8bd5cd8aed5316e0bb44f9797
Jujin Kim <jujin@jujinkim.com>
2022-03-27 16:40:06 +0900

    Add note repo interface to data module

---
+++ data/src/main/java/com/jujinkim/note/data/repo/NoteRepo.kt
@@ -0,0 +1,11 @@
+package com.jujinkim.note.data.repo
+
+import com.jujinkim.note.model.Note
+import com.jujinkim.note.model.NoteCategory
+
+interface NoteRepo {
+    fun getNote(id: String): Note
+    fun getNotes(catId: String): List<Note>
+    fun getCategory(catId: String): NoteCategory
+    fun getCategories(): List<NoteCategory>
+}
