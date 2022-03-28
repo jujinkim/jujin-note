c49d26a0f8cc3b50919441fe53dbefe3c86cd6b1
Jujin Kim <jujin@jujinkim.com>
2022-03-29 01:03:12 +0900

    Add icon int field into Category

--- data/src/main/java/com/jujinkim/note/data/entity/CategoryEntity.kt
+++ data/src/main/java/com/jujinkim/note/data/entity/CategoryEntity.kt
@@ -8,9 +8,10 @@ import com.jujinkim.note.model.NoteCategory
 @Entity(tableName = "category")
 data class CategoryEntity(
     @PrimaryKey val id: String,
-    @ColumnInfo(name = "title") val title: String
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "icon") val icon: Int
 )
 
-fun CategoryEntity.toModel() = NoteCategory(id, title)
+fun CategoryEntity.toModel() = NoteCategory(id, title, icon)
 
-fun NoteCategory.toEntity() = CategoryEntity(id, title)
+fun NoteCategory.toEntity() = CategoryEntity(id, title, icon)
--- model/src/main/java/com/jujinkim/note/model/NoteCategory.kt
+++ model/src/main/java/com/jujinkim/note/model/NoteCategory.kt
@@ -2,5 +2,6 @@ package com.jujinkim.note.model
 
 data class NoteCategory(
     val id: String,
-    val title: String
+    val title: String,
+    val icon: Int
 )
