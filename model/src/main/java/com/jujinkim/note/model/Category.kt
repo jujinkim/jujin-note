067509fc47c74023c6ef285273d93d409aea971f
Jujin Kim <jujin@jujinkim.com>
2022-03-30 16:36:10 +0900

    Add create new Category instance method at Category model

--- model/src/main/java/com/jujinkim/note/model/Category.kt
+++ model/src/main/java/com/jujinkim/note/model/Category.kt
@@ -1,7 +1,19 @@
 package com.jujinkim.note.model
 
+import java.math.BigInteger
+import java.security.MessageDigest
+
 data class Category(
     val id: String,
     val title: String,
     val icon: Int
-)
+) {
+    companion object {
+        fun new(title: String, icon: Int = 0) : Category {
+            val md5 = MessageDigest.getInstance("MD5")
+            val idInput = title + System.currentTimeMillis()
+            val id = BigInteger(1, md5.digest(idInput.toByteArray())).toString(16).padStart(32, '0')
+            return Category(id, title, icon)
+        }
+    }
+}
