c74e2901d0e8545b8c10e63ad628f5e5a2400758
Jujin Kim <jujin@jujinkim.com>
2022-04-22 19:51:47 +0900

    Add store dispatching codes at SettingViewModel.kt

--- ui/src/main/java/com/jujinkim/note/ui/setting/SettingViewModel.kt
+++ ui/src/main/java/com/jujinkim/note/ui/setting/SettingViewModel.kt
@@ -1,7 +1,11 @@
 package com.jujinkim.note.ui.setting
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.jujinkim.note.core.AppState
+import com.jujinkim.note.core.SettingAction
 import com.jujinkim.note.core.UiAction
 import dagger.hilt.android.lifecycle.HiltViewModel
 import org.reduxkotlin.Store
@@ -11,8 +15,26 @@ import javax.inject.Inject
 class SettingViewModel @Inject constructor(
     private val store: Store<AppState>
 ) : ViewModel() {
+    var currentSetting by mutableStateOf(store.state.setting)
+
+    private val unsubscribe = store.subscribe {
+        currentSetting = store.state.setting
+    }
 
     fun invokeBackToCategories() {
         store.dispatch(UiAction.NavigateToCategories)
+    }
+
+    fun invokeSaveSetting() {
+        store.dispatch(SettingAction.SaveSetting(currentSetting))
+    }
+
+    fun invokeLoadSetting() {
+        store.dispatch(SettingAction.LoadSetting)
+    }
+
+    override fun onCleared() {
+        unsubscribe()
+        super.onCleared()
     }
 }
