742ebef1eee5d3bdc2f44168c0dfb54581246748
Jujin Kim <jujin@jujinkim.com>
2022-04-23 21:10:02 +0900

    Add default expired date setting item at SettingCompose.kt (dialog not implemented yet)

--- ui/src/main/java/com/jujinkim/note/ui/setting/SettingCompose.kt
+++ ui/src/main/java/com/jujinkim/note/ui/setting/SettingCompose.kt
@@ -1,15 +1,19 @@
 package com.jujinkim.note.ui.setting
 
 import android.app.Activity
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Button
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.jujinkim.note.ui.BackHandler
 import com.jujinkim.note.ui.R
@@ -31,7 +35,7 @@ fun SettingContent(viewModel: SettingViewModel = hiltViewModel()) {
         topBar = { SettingTopBar() }
     ) {
         LazyColumn {
-
+            item { SettingItemExpiredDay() }
         }
 
     }
@@ -50,4 +54,32 @@ fun SettingTopBar(viewModel: SettingViewModel = hiltViewModel()) {
         Text(text = stringResource(id = R.string.settings))
     }
 
+}
+
+@Composable
+fun SettingItemExpiredDay(viewModel: SettingViewModel = hiltViewModel()) {
+    Column(
+        modifier = Modifier
+            .clickable { }
+            .wrapContentHeight()
+            .fillMaxWidth()
+    ) {
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = stringResource(id = R.string.settings_default_expired_date),
+            fontSize = 14.sp
+        )
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = stringResource(
+                id = if (viewModel.currentSetting.defaultExpiredDay > 0)
+                    R.string.settings_default_expired_date_after_pd_days
+                else
+                    R.string.settings_default_expired_date_permanent,
+                viewModel.currentSetting.defaultExpiredDay
+            ),
+            fontSize = 12.sp,
+            color = Color.Gray
+        )
+    }
 }
